e6f7ef680ba2e39bc628d6844e5653029d856379
Hyerin Choi <77471151+zzaerynn@users.noreply.github.com>
2022-03-23 00:57:23 +0900

    revised Sidebar Login header(subheader)

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -8,21 +8,26 @@ const Sidebar = ({ sidebarStatus, toggleSidebar }) => {
       <div className={sidebarStatus ? "sidebar" : "sidebar--inactive"}>
         <div className="sidebar__login">
           <div className="sidebar-login-header">
-            <p> Login </p>
+            <div> Login </div>
             <span className="sidebar__close" onClick={toggleSidebar}>
               x
             </span>
           </div>
-          <div> don't have an account? </div>
+          <div className = "sidebar-login-subheader"> don't have an account? </div>
         </div>
 
         <div className="sidebar-menu">
           <div className="sidebar-menu-winelist">
-            <span className="sidebar-menu-title">Title</span>
+            <div> </div>
+
+            <span className="sidebar-menu-title">Wine List</span>
             {/* 여기서 페이지 구현할때 sidebar__link 하나씩 복사해서 일단 사용 */}
             <div className="sidebar__link" onClick={toggleSidebar}>
               <Link to="/login">Login</Link>
             </div>
+
+
+
           </div>
         </div>
       </div>
