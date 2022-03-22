c32467c04272bc4f6cb9735df14f68c12c3ba088
Woohyun Park <woohyunpark.mail@gmail.com>
2022-03-23 01:53:37 +0900

    ADD, add routers and stlyes to header, footer, nav

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <>
-      <footer className="footer">Footer</footer>
+      <footer className="footer">
+        <div className="footer__title-cont">
+          <span className="footer__title">podo</span>
+        </div>
+        <div className="footer__cont">
+          <div className="footer__section">
+            HELP & SUPPORT
+            <div className="footer__link">
+              <Link to="/faq">FAQ</Link>
+            </div>
+            <div className="footer__link">
+              <Link to="/email-us">Email Us</Link>
+            </div>
+          </div>
+          <div className="footer__section">
+            SOCIAL
+            <div className="footer__link">
+              <Link to="/instagram">Instagram</Link>
+            </div>
+            <div className="footer__link">
+              <Link to="/facebook">Facebook</Link>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
