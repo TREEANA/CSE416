b3e6f173dbfac270477605a0ff6570db1126006e
Hyerin Choi <77471151+zzaerynn@users.noreply.github.com>
2022-03-28 02:05:43 +0900

    modified type = password and some margins for buttons

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -16,15 +16,17 @@ const Register = ({toggleRegisterModal, registerModalStatus}) => {
 
             <div className = "register__main">
                 <div className="register__title">Register</div>
-                <input className="register__email" placeholder = "email"></input>
-                <input className="register__name" placeholder = "username"></input>
-                <div classname = "register__name-warning"> dooddi is available</div>
+                <div className = "register__main-content">
+                    <input className="register__email" placeholder = "email"></input>
+                    <input className="register__name" placeholder = "username"></input>
+                    <div className = "register__name-warning"> dooddi is available</div>
 
-                <input className="register__pwd" placeholder = "password"></input>
-                <input className="register__pwd2" placeholder = "password"></input>
-                <div classname = "register__pwd-warning"> Password does not match! </div>
+                    <input className="register__pwd" placeholder = "password" type = "password"></input>
+                    <input className="register__pwd2" placeholder = "password" type = "password"></input>
+                    <div className = "register__pwd-warning"> Password does not match! </div>
 
-                <div className="register__register" onClick = {toggleRegisterModal} >register</div>
+                    <div className="register__register" onClick = {toggleRegisterModal} >register</div>
+                </div>
             </div>
         </div>
     </>
