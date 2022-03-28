eab8aad0581a5564b9a128c24ea4fd586f94c907
Hyerin Choi <77471151+zzaerynn@users.noreply.github.com>
2022-03-29 02:28:30 +0900

    merge 할때 ticket 조굼 지워져서 다시올려여

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import WinePage from "./components/WinePage/WinePage";
 import WineListPage from "./components/WineListPage/WineListPage";
 import FAQ from "./components/FAQ/FAQ";
 import Register from "./components/Register/Register";
+import Ticket from "./components/Ticket/Ticket"
+
 
 const App = () => {
   const [sidebarStatus, setSidebarStatus] = useState(0);
@@ -38,7 +40,13 @@ const App = () => {
     console.log("register tag modal toggled, now is ; ", registerTagModalStatus);
   }
 
+  const [ticketModalStatus, setTicketModalStatus] = useState(false);
 
+  const toggleTicketModal = () => {
+    setTicketModalStatus(!ticketModalStatus);
+    console.log("Ticket modal , current sortpage: ", ticketModalStatus);
+  }
+  
   const [searchBarStatus, setSearchBarStatus] = useState(false);
   const toggleSearchBar = () => {
     setSearchBarStatus(!searchBarStatus);
@@ -77,6 +85,7 @@ const App = () => {
         toggleLoginModal={toggleLoginModal}
         toggleRegisterModal = {toggleRegisterModal}
         toggleRegisterTagModal = {toggleRegisterTagModal}
+        toggleTicketModal={toggleTicketModal}
         userstatus={userstatus}
         filterpage = {filterpage}
         togglefilterpage = {togglefilterpage}
