9d59b24c8168782f6a3be9ee59fd0236ac572e7c
Hyerin Choi <77471151+zzaerynn@users.noreply.github.com>
2022-03-31 04:08:01 +0900

    detailed wine page done

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -9,6 +9,7 @@ import Review from "../Review/Review";
 import Wine from "../Wine/Wine";
 import WineList from "../WineList/WineList";
 import Tag from "../Tag/Tag";
+import {BsHeart, BsFillPencilFill, BsHeartFill, BsStar, BsStarFill} from 'react-icons/bs';
 
 const Detail = () => {
   
@@ -35,12 +36,63 @@ const Detail = () => {
               <div className="detail__wine-price">₩17,000</div>
             </div>
 
+
+            
           </div>
-          
+
+          {/* <div className="detail__wine-taste">
+            <table>
+              <tr className = "detail__wine-char">
+                <td className = "detail__wine-char1">light</td>
+                <td className = "detail__wine-range"> 
+                  <div className = "detail__wine-range-div">
+                    <span className = "detail__wine-range-span">  </span>
+                  </div>
+                </td>
+                <td className = "detail__wine-char2">bold</td>
+              </tr>  
+            </table>
+          </div> */}
+
         </div>
         <hr className = "detail__line"></hr>
           <div className = "detail__review">
             <div className = "detail__review-title"> Reviews </div>
+
+            <div className = "detail__one-review">
+              <div className = "detail__review-title">
+                <div className = "detail__review-star"> <BsStarFill/> <BsStarFill/> <BsStarFill/> <BsStarFill/> <BsStar/> </div>
+                <div className = "detail__review-icons">
+                  <BsHeartFill/>  
+                  <BsFillPencilFill/>
+                </div>
+              </div>
+
+              <div className = "detail__review-addtag">
+                <div className = "detail__review-tagcont"> 
+                  <input className = "detail__review-input" placeholder = "add tags "></input>
+                  <div className = "detail__review-plus"> +</div>
+                </div>
+                <div>
+                  <Tag isDisabled = {0} isFilled = {1} txt = "x acidic"></Tag>
+                  <Tag isDisabled = {0} isFilled = {1} txt = "x dry"></Tag>
+                  <Tag isDisabled = {0} isFilled = {1} txt = "x light"></Tag>
+                  <Tag isDisabled = {0} isFilled = {1} txt = "x steak"></Tag>
+                  <Tag isDisabled = {0} isFilled = {1} txt = "x picnic"></Tag>
+                </div>
+              </div>
+
+              <div className = "detail__review-content">
+                  Great autumn wine. Clean leather, mint, cherry, blackberry and chocolate. Worth opening ahead of drinking - smooth and mellow.
+              </div>
+              <div className = "detail__review-post"> post a review </div>
+              <div>
+
+              </div>
+            </div>
+
+
+
             <Review userstatus={1}/>
             <Review userStatus={0}/>
           </div>
