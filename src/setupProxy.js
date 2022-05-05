29b40a069fd2d2ff87ec439de632320a9fd0efe0
Woohyun Park <woohyunpark.mail@gmail.com>
2022-05-06 00:51:25 +0900

    FIX, fix createPage but still needed test with backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const API_KEY_CURRENCY = "b158cab8f2dc4fc3be4972f79b642122";
+const API_KEY_IMAGE = "dx0q9mebc";
 
 module.exports = (app) => {
   app.use(
@@ -16,6 +17,14 @@ module.exports = (app) => {
     createProxyMiddleware({
       target: `https://api.currencyfreaks.com/latest?apikey=${API_KEY_CURRENCY}`,
       pathRewrite: { "^/external/currency": "" },
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    "/external/image",
+    createProxyMiddleware({
+      target: `https://api.cloudinary.com/v1_1/${API_KEY_IMAGE}/image/upload`,
+      pathRewrite: { "^/external/image": "" },
       changeOrigin: true,
     })
   );
