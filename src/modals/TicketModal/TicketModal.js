9fc704b99b923282616bd7dc87ca72c38edd4bf2
Hyerin Choi <77471151+zzaerynn@users.noreply.github.com>
2022-05-07 02:26:49 +0900

    IN PROGRESS, making 'create a new ticket' here.

--- src/modals/TicketModal/TicketModal.js
+++ src/modals/TicketModal/TicketModal.js
@@ -11,6 +11,17 @@ import {
 import Ticket from "../../components/Ticket/Ticket";
 
 const TicketModal = ({ ticketModalStatus, toggleTicketModal }) => {
+  const [tempTicket, setTempTicket] = useState({
+    visible: false,
+    ticketTitle: "",
+    ticketContent: "",
+    lastUpdatedAt: "",
+  });
+
+  const toggleTempTicket = () => {
+    setTempTicket(!tempTicket.visible);
+  };
+
   return (
     <>
       <div
@@ -25,9 +36,37 @@ const TicketModal = ({ ticketModalStatus, toggleTicketModal }) => {
             />
           </div>
           <div className="ticketModal__create">
-            <BsFillPlusCircleFill /> create a new ticket
+            <BsFillPlusCircleFill onClick={toggleTempTicket} />
+            create a new ticket
           </div>
-
+          {/* {tempTicket.visible && ( */}
+          <>
+            <div className="ticketModal__temp">
+              <div className="ticketModal__title">
+                <div className="ticketModal__titleTitle">New Ticket</div>
+                <button className="ticketModal__tempSubmit">Submit</button>
+              </div>
+              {/* <hr></hr> */}
+              <div className="ticketModal__tempTitle">
+                <div className="ticketModal__tempTitleTitle">Title </div>
+                <input
+                  type="text"
+                  className="ticketModal__tempTitleInput"
+                  placeholder="Title"
+                ></input>
+              </div>
+              <hr></hr>
+              <div className="ticketModal__tempContent">
+                <div className="ticketModal__tempContentTitle">Content</div>
+                <input
+                  type="text"
+                  className="ticketModal__tempContentInput"
+                  placeholder="Have any issues? Feel free to report it to us! \n 사실 지금 새벽 두시반.. 졸라리 늦은ㅅ ㅣ간.. 눈이 너무 감기는데 한게 없어서 감기면 안돼.. 진짜로..."
+                ></input>
+              </div>
+            </div>
+          </>
+          {/* )} */}
           <Ticket
             type="ticket"
             ticketStatus={0}
