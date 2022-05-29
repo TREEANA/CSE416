3aa2bd013ba348d74443860d388cd38ae098921c
Hyerin Choi <77471151+zzaerynn@users.noreply.github.com>
2022-05-30 05:45:15 +0900

    PROGRESS, Manage Ticket Modal for admin

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -1,78 +1,107 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Ticket.css";
 import {
-  BsXLg,
   BsFilePlusFill,
   BsFillCheckCircleFill,
   BsThreeDots,
 } from "react-icons/bs";
 
-// const ticketDummyData = {};
+const ticketDummyData = {
+  ticketID: 1,
+  userID: 1,
+  username: "zzaerynn",
+  adminID: 9999,
+  title: "I cannot add winelists to my favorites",
+  userQuestion:
+    "Everytime I click the add to favorites button, it somehow does not work",
+  adminResponse:
+    "This is now fixed! try it again and tell us if it still doesnt work",
+  createdAt: "2022-04-04 18:11:12",
+  lastUpdatedAt: "2022-04-04 18:11:12",
+};
 
-const Ticket = ({ type, ticketStatus, title, question, answer }) => {
+const Ticket = ({ type, ticketData = ticketDummyData }) => {
+  const [tempTicket, setTempTicket] = useState({});
+
   const [isOpen, setIsOpen] = useState(false);
   const onClick = () => {
     setIsOpen(!isOpen);
   };
+
+  //ticket status (0 : not answered (pending, yellow), 1: answered (green))
+  const [ticketStatus, setTicketStatus] = useState(0);
+  //load status for each ticket when page is initially loaded
+  useEffect(() => {
+    if (ticketData.adminID === "null") {
+      setTicketStatus(0);
+    } else {
+      setTicketStatus(1);
+    }
+    setTempTicket(ticketData);
+  }, []);
+
   const displayTicketStatus = () => {
     if (type === "faq") {
       return;
     } else if (type === "ticket") {
       if (ticketStatus === 0) {
         return (
+          //pending
           <div className="ticket__progress ticket__progress--0">
-            <BsFillCheckCircleFill />
+            <BsThreeDots />
           </div>
         );
       } else if (ticketStatus === 1) {
         return (
+          // approved(answered)
           <div className="ticket__progress ticket__progress--1">
-            <BsThreeDots />
+            <BsFillCheckCircleFill />
           </div>
         );
       }
     }
   };
-  const displayAnswer = () => {
-    if (type === "faq") {
-      return (
-        <div className="ticket__answer">
-          <b>A.</b>
-          {answer}
-        </div>
-      );
-    } else if (type === "ticket") {
-      if (ticketStatus === 0) {
-        return (
-          <div className="ticket__answer">
-            <b>A.</b>
-            {answer}
-          </div>
-        );
-      }
-    } else if (type === "verify") {
-      if (ticketStatus === 0) {
-        return (
-          <div className="ticket__answer">
-            <b>{"hella assignment"}</b>
-          </div>
-        );
-      }
-    }
-  };
+
+  // const displayAnswer = () => {
+  //   if (type === "faq") {
+  //     return (
+  //       // <div className="ticket__answer">
+  //         {tempTicket.adminResponse}
+  //       // </div>
+  //     );
+  //   } else if (type === "ticket") {
+  //     if (ticketStatus === 0) {
+  //       return (
+  //         // <div className="ticket__answer">
+
+  //           {tempTicket.adminResponse}
+  //         {/* </div> */}
+  //       );
+  //     }
+  //     //verification ticket은 다른 모달/페이지/컴포 사용
+  //     // } else if (type === "verify") {
+  //     //   if (ticketStatus === 0) {
+  //     //     return (
+  //     //       <div className="ticket__answer">
+  //     //         <b>{"hella assignment"}</b>
+  //     //       </div>
+  //     //     );
+  //     //   }
+  //   }
+  // };
   return (
     <div className="ticket__cont">
       <div
         className={
           type === "faq"
             ? "ticket__button ticket__button--faq"
-            : type === "verify"
-            ? "ticket__button ticket__verify"
-            : "ticket__button"
+            : // : type === "verify"
+              // ? "ticket__button ticket__verify"
+              "ticket__button"
         }
         onClick={onClick}
       >
-        <div className="ticket__buttonTxt">{title}</div>
+        <div className="ticket__buttonTxt">{tempTicket.title}</div>
         {displayTicketStatus()}
       </div>
       <div
@@ -80,13 +109,19 @@ const Ticket = ({ type, ticketStatus, title, question, answer }) => {
       >
         <div className="ticket__question">
           <b>Q</b>
-          {question}
+          {tempTicket.userQuestion}
         </div>
         <hr></hr>
-        {displayAnswer()}
+        <div className="ticket__answer">
+          <b>A.</b>
+          <textarea className="ticket__answerInput">
+            {tempTicket.adminResponse !== "" ? tempTicket.adminResponse : ""}
+          </textarea>
+        </div>
       </div>
     </div>
   );
 };
+``;
 
 export default Ticket;
---
+++ src/modals/TicketAdminModal/TicketAdminModal.js
@@ -0,0 +1,130 @@
+import "./TicketAdminModal.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Ticket from "../../components/Ticket/Ticket";
+import {
+  BsXLg,
+  BsFilePlusFill,
+  BsFillCheckCircleFill,
+  BsThreeDots,
+  BsFillPlusCircleFill,
+} from "react-icons/bs";
+
+const TicketAdminModal = ({ status, toggleStatus }) => {
+  //support tickets retreived from adminID
+  const [supportTickets, setSupportTickets] = useState({});
+
+  //userID of Admin: fetched from status
+  const userID = status.userID;
+  //fetch tickets from db
+  const fetchAdminTickets = async (userID) => {
+    try {
+      const res = await axios.get(`/api/support-tickets/?userID=${userID}`);
+      console.log("res.data from fetchAdminTickets: ", res.data);
+      setSupportTickets(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchAdminTickets(userID);
+  }, []);
+
+  //refers to "status" in api calls
+  // 0 (all the tickets), 1(answered tickets), 2(pending tickets)
+  const [viewStatus, setViewStatus] = useState(0);
+
+  const displayAdminTickets = (viewStatus) => {
+    let result = [];
+    if (viewStatus === 0) {
+      supportTickets.forEach((each, index) => {
+        result.push(<Ticket type="ticket" ticketData={each} key={index} />);
+      });
+    } else if (viewStatus === 1) {
+      supportTickets.forEach((each, index) => {
+        if (each.adminID !== null) {
+          result.push(<Ticket type="ticket" ticketData={each} key={index} />);
+        }
+      });
+    } else if (viewStatus === 2) {
+      supportTickets.forEach((each, index) => {
+        if (each.adminID === null) {
+          result.push(<Ticket type="ticket" ticketData={each} key={index} />);
+        }
+      });
+    }
+    return result;
+  };
+
+  return (
+    <>
+      <div
+        className={
+          status.ticketAdminModal ? "ticketAdmin" : "ticketAdmin--inactive"
+        }
+      >
+        <div className="ticketAdmin__container">
+          <div className="ticketAdmin__header">
+            <div className="ticketAdmin__title"> Manage Tickets</div>
+            <BsXLg
+              className="ticketAdmin__close"
+              onClick={() => {
+                toggleStatus("ticketAdminModal");
+              }}
+            />
+          </div>
+          <div className="ticketAdmin__status">
+            <div className="ticketAdmin__statusIcon">
+              <button
+                className={
+                  viewStatus === 0
+                    ? "ticketAdmin__statusButton--active ticketAdmin__statusButton"
+                    : "ticketAdmin__statusButton"
+                }
+                onClick={() => {
+                  setViewStatus(1);
+                }}
+              >
+                show all
+              </button>
+            </div>
+            <div className="ticketAdmin__statusIcon">
+              <button
+                className={
+                  viewStatus === 1
+                    ? "ticketAdmin__statusButton--active ticketAdmin__statusButton"
+                    : "ticketAdmin__statusButton"
+                }
+                onClick={() => {
+                  setViewStatus(1);
+                }}
+              >
+                show only answered
+              </button>
+            </div>
+            <div className="ticketAdmin__statusIcon">
+              <button
+                className={
+                  viewStatus === 2
+                    ? "ticketAdmin__statusButton--active ticketAdmin__statusButton"
+                    : "ticketAdmin__statusButton"
+                }
+                onClick={() => {
+                  setViewStatus(2);
+                }}
+              >
+                show only pending
+              </button>
+            </div>
+          </div>
+
+          <div className="ticketAdmin__tickets">
+            {displayAdminTickets(viewStatus)}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TicketAdminModal;
